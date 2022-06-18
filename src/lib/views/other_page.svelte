afd9e69c01b727cc9652e6d808c1bfe27a131c78
martin444 <alejandrofarel62@gmail.com>
2022-06-18 13:01:18 -0300

    add new font sized

--- src/lib/views/other_page.svelte
+++ src/lib/views/other_page.svelte
@@ -164,7 +164,7 @@
         font-family: monospace;
         white-space: nowrap;
         text-align: center;
-        font-size: 3vmin;
+        font-size: 4vmin;
         margin: 0;
         width: 30ch;
         animation: typing 2s steps(40), blink .5s infinite step-end alternate;
